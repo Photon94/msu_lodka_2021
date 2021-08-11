2d29f6dc823126f6a9e14efda696f970e55c7ef4
Photon94 <299792458.photon.94@gmail.com>
2021-08-11 18:02:34 +1000

    updated settings, added readme

---
+++ README.md
@@ -0,0 +1,7 @@
+## install
+
+Для установки `pymurapi` используя `pip` выполни следующее:
+ 
+```bash
+pip install git+https://github.com/murproject/pymurapi.git
+```
--- settings.py
+++ settings.py
@@ -4,7 +4,16 @@ YELLOW_RANGE = Range(low=(15, 50, 50), high=(45, 255, 255))
 RED_RANGE = [Range(low=(0, 50, 50), high=(15, 255, 255)), Range(low=(175, 50, 50), high=(180, 255, 255))]
 GREEN_RANGE = Range(low=(45, 50, 50), high=(125, 255, 255))
 
-# todo calibrate s and v
 BLUE_RANGE = Range(low=(105, 50, 50), high=(135, 255, 255))
 
 SIMULATOR = True
+
+# пороговое значение для определения контуров
+CONTOURS_SENSITIVE = 30
+
+# эпсилон при сравнении контуров
+CONTOURS_EPSILON_PERCENT = 1
+
+# угол обзора камеры
+CAMERA_VIEWING_ANGLE = 50
+
